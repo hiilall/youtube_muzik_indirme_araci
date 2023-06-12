c601dc75717ea6f82ac81a81f1780aaeb03444aa
H' lal <ehazca@gmail.com>
2023-06-12 17:58:05 +0300

    Update DONUSTUR.py

--- DONUSTUR.py
+++ DONUSTUR.py
@@ -1,7 +1,7 @@
 import os
 
 try:
-    os.system("python -m PyQt5.uic.pyuic -x thresholdV2.ui -o ui_thresholdV2.py")
+    os.system("python -m PyQt5.uic.pyuic -x downloadTool.ui -o ui_downloadTool.py")
     #☻os.system("pyrcc5 icons.qrc -o icons_rc.py")
 except Exception as e:
     print(e)
